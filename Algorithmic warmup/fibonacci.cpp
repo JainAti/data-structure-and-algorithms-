e76f2dd545c5fb21d9a6134c353e16248644dbaf
JainAti <66514345+JainAti@users.noreply.github.com>
2020-07-22 17:54:34 +0530

    Update fibonacci.cpp

--- Algorithmic warmup/fibonacci.cpp
+++ Algorithmic warmup/fibonacci.cpp
@@ -2,20 +2,11 @@
 #include <cassert>
 #include<bits/stdc++.h>
 using namespace std;
+/*Task. Given an integer n, find the last digit of the nth Fibonacci number F n (that is, F n mod 10).
+Input Format. The input consists of a single integer n.
+Constraints. 0 ≤ n ≤ 10 7 .
+Output Format. Output the last digit of F n .*/
 
-
-// The following code calls a naive algorithm for computing a Fibonacci number.
-//
-// What to do:
-// 1. Compile the following code and run it on an input "40" to check that it is slow.
-//    You may also want to submit it to the grader to ensure that it gets the "time limit exceeded" message.
-// 2. Implement the fibonacci_fast procedure.
-// 3. Remove the line that prints the result of the naive algorithm, comment the lines reading the input,
-//    uncomment the line with a call to test_solution, compile the program, and run it.
-//    This will ensure that your efficient algorithm returns the same as the naive one for small values of n.
-// 4. If test_solution() reveals a bug in your implementation, debug it, fix it, and repeat step 3.
-// 5. Remove the call to test_solution, uncomment the line with a call to fibonacci_fast (and the lines reading the input),
-//    and submit it to the grader.
 
 int fib(int n) 
 { 
